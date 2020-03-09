89d8a257ba091990eb5e3432febcd56c3e613a46
anvesh-g <anvesh.gatadi@gmail.com>
2020-03-09 10:31:43 +0530

    few more methods

--- src/com/set_1/go/classes/SimpleLambdaExpressions.java
+++ src/com/set_1/go/classes/SimpleLambdaExpressions.java
@@ -5,9 +5,8 @@ import com.set_1.go.interfaces.ConstructorRef;
 import com.set_1.go.interfaces.LengthOfString;
 
 import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
@@ -55,8 +54,73 @@ public class SimpleLambdaExpressions {
         System.out.println(convertListToArray());
         //Sort map based on keys
         System.out.println(sortMapBasedOnKeys());
+        //get Element count in the list
+        System.out.println(getElementCount(25));
+        //difference between two lists
+        System.out.println(getDifferenceBetweenTwoLists());
+        //get distinct values from list
+        System.out.println(getDistinctValuesFromList());
+        //return elements which satisfies the predicate
+        IntPredicate condition = a-> a>5;
+        System.out.println(getElementsSatisfyingTheCondition(condition));
+        //get duplicate values from list
+        BiPredicate<List<Integer>,Integer> p = (List<Integer> list,Integer element) -> list.stream().filter(i -> i == element).count() > 1;
+        System.out.println(getDuplicateValuesFromList(p));
+
+        //get Duplicate elements with count
+        System.out.println(getDuplicateElementsWithCount());
+
+        //sort characters in string
+        System.out.println(sortCharactersInString("anvesh"));
+        //alternate Uppercase lowercase in string
+        System.out.println(alternateUpperLowerString("anvesh"));
+        //count lower case letters in a string
+        System.out.println(getCountOfLowerCaseLetters("AnvesH"));
 
     }
+
+    public static int getCountOfLowerCaseLetters(String text){
+//        Arrays.stream(text.toCharArray()).filter(i-> i >= 97 && i <= 122).count();
+        return 0;
+    }
+
+    public static String alternateUpperLowerString(String text){
+        AtomicInteger count= new AtomicInteger();
+        return Arrays.stream(text.split("")).map(i-> count.get() % 2 == 0 ? text.charAt(count.get())+"".toUpperCase() : text.charAt(count.get())+"".toLowerCase()).peek(i-> count.getAndIncrement())
+                .collect(Collectors.joining());
+    }
+
+
+    public static String sortCharactersInString(String input) {
+        return Arrays.stream(input.split("")).sorted().collect(Collectors.joining());
+    }
+
+    public static Map<Integer,Long> getDuplicateElementsWithCount(){
+        return getElements().stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+    }
+
+    public static List<Integer> getDuplicateValuesFromList(BiPredicate<List<Integer>,Integer> p){
+        return getElements().stream().filter(i->p.test(getElements(),i)).distinct().collect(Collectors.toList());
+
+    }
+
+
+    public static List<Integer> getElementsSatisfyingTheCondition(IntPredicate condition){
+        return getElements().stream().filter(a->condition.test(a)).collect(Collectors.toList());
+    }
+
+    public static List<Integer> getDistinctValuesFromList(){
+        return getElements().stream().distinct().collect(Collectors.toList());
+    }
+
+    public static List<Integer> getDifferenceBetweenTwoLists(){
+        return getElements2().stream().filter(a->getElements().contains(a)).collect(Collectors.toList());
+    }
+
+    public static long getElementCount(int element){
+        return getElements().stream().filter(a->a==element).count();
+    }
+
 
     public static int addition(int a, int b) {
         Add add = (x, y) -> x + y;
@@ -177,6 +241,19 @@ public class SimpleLambdaExpressions {
         list.add(0);
         list.add(15);
         list.add(5);
+        list.add(25);
+        list.add(25);
+        return list;
+    }
+
+    public static List<Integer> getElements2() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(30);
+        list.add(0);
+        list.add(15);
+        list.add(5);
+        list.add(45);
         list.add(25);
         return list;
     }
